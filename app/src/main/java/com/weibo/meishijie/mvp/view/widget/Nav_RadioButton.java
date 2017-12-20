97d82b582bc574522fdc564f7b0886446b605297
wxjqgt <gt1254094162@163.com>
2017-12-20 21:58:49 +0800

    建立APP基本架构！

---
+++ app/src/main/java/com/weibo/meishijie/mvp/view/widget/Nav_RadioButton.java
@@ -0,0 +1,85 @@
+package com.weibo.meishijie.mvp.view.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.util.AttributeSet;
+
+import com.weibo.meishijie.R;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class Nav_RadioButton extends AppCompatRadioButton {
+
+    /**
+     * xml文件中设置的大小
+     */
+    private int mDrawableSize;
+
+    public Nav_RadioButton(Context context) {
+        this(context, null, 0);
+    }
+
+    public Nav_RadioButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public Nav_RadioButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        Drawable drawableLeft = null, drawableTop = null,
+                drawableRight = null, drawableBottom = null;
+        TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.NavigationBar_RadioButton);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.NavigationBar_RadioButton_DrawableSize:
+                    mDrawableSize = a.getDimensionPixelSize(
+                            R.styleable.NavigationBar_RadioButton_DrawableSize, 0);
+                    break;
+                case R.styleable.NavigationBar_RadioButton_DrawableTop:
+                    drawableTop = a.getDrawable(attr);
+                    break;
+                case R.styleable.NavigationBar_RadioButton_DrawableRight:
+                    drawableRight = a.getDrawable(attr);
+                    break;
+                case R.styleable.NavigationBar_RadioButton_DrawableBottom:
+                    drawableBottom = a.getDrawable(attr);
+                    break;
+                case R.styleable.NavigationBar_RadioButton_DrawableLeft:
+                    drawableLeft = a.getDrawable(attr);
+                    break;
+                default:
+                    break;
+            }
+        }
+        //回收
+        a.recycle();
+        setCustomCompoundDrawablesWithIntrinsicBounds(
+                drawableLeft, drawableTop, drawableRight, drawableBottom);
+    }
+
+    public void setCustomCompoundDrawablesWithIntrinsicBounds(
+            Drawable left, Drawable top, Drawable right, Drawable bottom) {
+        if (mDrawableSize == 0){
+            mDrawableSize = 30;
+        }
+        if (left != null) {
+            left.setBounds(0, 0, mDrawableSize, mDrawableSize);
+        }
+        if (right != null) {
+            right.setBounds(0, 0, mDrawableSize, mDrawableSize);
+        }
+        if (top != null) {
+            top.setBounds(0, 0, mDrawableSize, mDrawableSize);
+        }
+        if (bottom != null) {
+            bottom.setBounds(0, 0, mDrawableSize, mDrawableSize);
+        }
+        setCompoundDrawables(left, top, right, bottom);
+    }
+}
