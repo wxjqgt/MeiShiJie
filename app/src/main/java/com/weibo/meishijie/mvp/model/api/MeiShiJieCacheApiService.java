47291440b219ce7741a3eb524cd2d02d54475303
wxjqgt <gt1254094162@163.com>
2017-12-29 00:34:59 +0800

    获取home_recommend页面的数据！

--- app/src/main/java/com/weibo/meishijie/mvp/model/api/MeiShiJieCacheApiService.java
+++ app/src/main/java/com/weibo/meishijie/mvp/model/api/MeiShiJieCacheApiService.java
@@ -5,17 +5,27 @@ import com.weibo.meishijie.bean.home_recommend.HomeRecommend;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.rx_cache2.Encrypt;
+import io.rx_cache2.EncryptKey;
+import io.rx_cache2.EvictProvider;
 import io.rx_cache2.LifeCache;
+import io.rx_cache2.Migration;
 import io.rx_cache2.Reply;
+import io.rx_cache2.SchemeMigration;
 
 /**
  * Created by Administrator on 2017/12/28.
  */
 
+@SchemeMigration({
+        @Migration(version = 1, evictClasses = {HomeRecommend.class})
+})
+@EncryptKey("meishijie_cache_provides")
 public interface MeiShiJieCacheApiService {
     /**
      * @return 推荐页的数据，使用rxcahe要求包装返回
      */
-    @LifeCache(duration = 3,timeUnit = TimeUnit.MINUTES)
-    Observable<Reply<HomeRecommend>> requestHomeRecommendData(Observable<HomeRecommend> homeRecommendObservable);
+    @Encrypt
+    @LifeCache(duration = 3, timeUnit = TimeUnit.MINUTES)
+    Observable<Reply<HomeRecommend>> requestHomeRecommendData(Observable<HomeRecommend> homeRecommendObservable, EvictProvider evictProvider);
 }
--- app/src/main/java/com/weibo/meishijie/mvp/view/fragment/RecommendFragment.java
+++ app/src/main/java/com/weibo/meishijie/mvp/view/fragment/RecommendFragment.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
+import io.rx_cache2.EvictProvider;
 import io.rx_cache2.Reply;
 
 
@@ -42,7 +43,7 @@ public class RecommendFragment extends BaseFragment {
 
     @Override
     protected void loadData() {
-        meiShiJieCacheApiService.requestHomeRecommendData(meiShiJieApiService.requestHomeRecommendData())
+        meiShiJieCacheApiService.requestHomeRecommendData(meiShiJieApiService.requestHomeRecommendData(), new EvictProvider(true))
                 .compose(bindToLifecycle())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
