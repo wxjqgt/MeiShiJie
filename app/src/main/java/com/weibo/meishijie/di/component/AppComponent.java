53040e7a865af56c8d3fda673adda9a401653888
wxjqgt <gt1254094162@163.com>
2018-01-03 23:04:45 +0800

    重构

--- app/src/main/java/com/weibo/meishijie/di/component/AppComponent.java
+++ app/src/main/java/com/weibo/meishijie/di/component/AppComponent.java
@@ -3,10 +3,15 @@ package com.weibo.meishijie.di.component;
 import com.weibo.meishijie.di.module.AppModule;
 import com.weibo.meishijie.feature.glide.integration.OkHttpLibraryGlideModule;
 import com.weibo.meishijie.mvp.model.RecommendModelImlp;
+import com.weibo.meishijie.mvp.model.api.MeiShiJieApiService;
+import com.weibo.meishijie.mvp.model.api.MeiShiJieCacheApiService;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
+import io.rx_cache2.internal.RxCache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
 
 /**
  * Created by Administrator on 2018/1/1.
@@ -15,6 +20,11 @@ import dagger.Component;
 @Singleton
 @Component(modules = AppModule.class)
 public interface AppComponent {
+    OkHttpClient providesOkHttpClient();
+    RxCache providesRxCache();
+    Retrofit providesRetrofit();
+    MeiShiJieCacheApiService providesCacheApi();
+    MeiShiJieApiService providesApi();
     void inject(RecommendModelImlp recommendModelImlp);
     void inject(OkHttpLibraryGlideModule okHttpLibraryGlideModule);
 }
